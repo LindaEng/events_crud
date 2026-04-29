41d11e34713c60f5b0706637c5484c4465eb767f
LindaEng <90lineng@gmail.com>
2026-04-29 18:35:35 -0400

    react context updated

--- frontend/src/Events.tsx
+++ frontend/src/Events.tsx
@@ -1,35 +1,47 @@
 import { useState, useEffect } from 'react'
+import { useEventsContext } from './hooks/EventHooks';
 import { Link } from "react-router-dom"
 import './App.css'
 import type { EventType } from "./types/EventType";
 
 function Events() {
-  const [events, setEvents] = useState<EventType[]>([])
+  // const [events, setEvents] = useState<EventType[]>([])
+  const {events} = useEventsContext()
+  const [filteredEvents, setFilteredEvents] = useState<EventType[]>([])
   const [locations, setLocations] = useState<string[]>([])
   const [filteredLoc, setFilteredLoc] = useState<string>("")
   const [keyword, setKeyword] = useState<string>("")
   const [debouncedKeyword, setDebouncedKeyword] = useState(keyword)
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const locationQuery = filteredLoc ? `?location=${encodeURIComponent(filteredLoc)}` : "";
-      const keywordQuery = debouncedKeyword ? `${locationQuery ? "&" : "?"}keyword=${encodeURIComponent(debouncedKeyword)}` : "";
-      try {
-        const response = await fetch(`http://localhost:3000/events${locationQuery}${keywordQuery}`)
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setEvents(data);
+
+  // useEffect(() => {
+  //   const fetchEvents = async () => {
+  //     const locationQuery = filteredLoc ? `?location=${encodeURIComponent(filteredLoc)}` : "";
+  //     const keywordQuery = debouncedKeyword ? `${locationQuery ? "&" : "?"}keyword=${encodeURIComponent(debouncedKeyword)}` : "";
+  //     try {
+  //       const response = await fetch(`http://localhost:3000/events${locationQuery}${keywordQuery}`)
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       const data = await response.json();
+  //       setEvents(data);
         
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    fetchEvents();
-    findLocations();
-  }, [filteredLoc, debouncedKeyword]) //Why is our console printing infinitely
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+  //   fetchEvents();
+  //   findLocations();
+  // }, [filteredLoc, debouncedKeyword]) //Why is our console printing infinitely
     //helper
+
+  useEffect(() => {
+    findLocations();
+  },[debouncedKeyword])
+
+  useEffect(() => {
+    setFilteredEvents([...events]);
+  }, [events]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -40,13 +52,8 @@ function Events() {
 
   const findLocations = async () => {
     try {
-       const response = await fetch(`http://localhost:3000/events`)
-       if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-      }
-        const data = await response.json();
         const seen = new Set(); 
-        data.map((event:any) => {
+        filteredEvents.map((event:any) => {
           if(!seen.has(event.location)) {
             seen.add(event.location)
             return event.location
@@ -57,14 +64,35 @@ function Events() {
       console.error(error)
     }
   }
+
+  const filteredResults = (keyword: string, location: string) => {
+    const filteredEvents = events.filter(event => {
+      const matchesLocation = !location || event.location === location;
+      const matchesKeyword = !keyword || event.title.toLowerCase().includes(keyword.toLowerCase());
+      return matchesLocation && matchesKeyword;
+    });
+
+    setFilteredEvents(filteredEvents);
+  }
+
+
   const handleLocChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
       setFilteredLoc(e.target.value)
+      filteredResults(debouncedKeyword, e.target.value)
   }
-  console.log("KEYWORD ", keyword)
+
+
+  const handleKeywordChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
+      setKeyword(e.target.value)
+      filteredResults(e.target.value, filteredLoc)
+  }
+  
   return (
     <>
       {/* text based search */}
-      <input value={keyword} type="text" placeholder='Search with keyword' onChange={(e) => setKeyword(e.target.value)}/>
+      <input value={keyword} type="text" placeholder='Search with keyword' onChange={(e) => 
+        handleKeywordChange(e)
+      }/>
       {/* Filter */}
       <select value={filteredLoc} onChange={(e) => handleLocChange(e)}>
         <option value="">All locations</option>
@@ -72,7 +100,7 @@ function Events() {
           <option key={location} value={location}>{location}</option>
         ))}
       </select>
-        {events.map((event) => (
+        {filteredEvents.map((event) => (
           <div key={event.id}>
               <Link to={`/events/${event.id}`}><h3>{event.title}</h3></Link>
           </div>
